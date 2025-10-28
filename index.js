31bcc7c81f9390923bdb67c16baf6da21614f691
MiguelPachon <miguel.pachon@estudiantesunibague.edu.co>
2025-10-27 21:04:20 -0500

    endpoint creado

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require("express")
+const comicsRouter = require("./routes/comics")
 
 const app = express()
 
 const port = 3000
 
 app.use(express.json())
+
+app.use("/comics", comicsRouter)
 
 app.listen(port, () => {
     console.log("Server running on port: ", port)
